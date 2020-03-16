3eca94b4262afbc5c442686c5a2bb138e2f9e34c
jOh <j0h@example.com>
2020-03-17 02:43:20 +0800

    conditionally run effects

--- src/Hooks/Hookcounter.js
+++ src/Hooks/Hookcounter.js
@@ -2,15 +2,19 @@ import React, { useState,useEffect } from 'react';
 
 function Hookcounter() {
   
-    const[count,setCount] = useState(0)
+    const [count,setCount] = useState(0)
+    const [name , setName] = useState('')
     
-    useEffect(()=>{
+    
+     useEffect(()=>{
+         console.log('updating')
         document.title = 'you Click'+count+'times'
-    }
+        },[count]
     )
 
         return (
             <div>
+                <input type="text" value={name} onChange={ (e)  => setName(e.target.value)}/> 
                 <button onClick={()=>setCount(count+1)}>Count {count}</button>
             </div>
         )
